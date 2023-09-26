04cdf045b58698b9781b86efe68bbcc42a2469b7
Aiiii-den <becker.work@web.de>
2023-09-26 10:33:55 +0200

    cors header added

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = 8082;
 const cors = require('cors')
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use('/prompt', promptRoutes);
 app.use('/random', randomPromptRoutes);
 
